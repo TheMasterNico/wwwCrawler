5813b3d631b633250536433244e754ba94c90e3c
Nicolas Castillo (TheMasterNico) <nscastillo.h@gmail.com>
2020-08-01 18:36:03 -0500

    Change small things in the calling of function

--- routes/index.js
+++ routes/index.js
@@ -3,38 +3,31 @@ let router = express.Router();
 
 let funcMongo = require('../api/mongo');
 
-router.get('/', (req, res, next) => {
-	funcMongo.GetCategories(req.params.category).then((result) => {
-		res.render('categories', { categories: result, title: 'Comparando precios' });
-	});
+router.get('/', async (req, res, next) => {
+	const result = await funcMongo.GetCategories(null);
+	res.render('categories', { categories: result, title: 'Comparando precios' });
 });
 
-router.get('/cat/:category(*)', (req, res) => {
-	funcMongo.GetCategories(req.params.category).then((result) => {
-		if (result.length) { // have categories
-			//console.log(result);
-			res.render('categories', { categories: result, title: 'Comparando precios' });
-		}
-		else {
-			funcMongo.GetProducts(req.params.category).then((result_prod) => {
-				//console.log(result_prod);
-				res.render('products', { prod: result_prod, title: req.params.category });
-			});
-		}
-	});
+router.get('/cat/:category(*)', async (req, res) => {
+	const result = await funcMongo.GetCategories(req.params.category);
+	if (result.length) { // have more categories
+		res.render('categories', { categories: result, title: 'Comparando precios' });
+	}
+	else {
+		const result_prod = await funcMongo.GetProducts(req.params.category);
+		res.render('products', { prod: result_prod, title: req.params.category });
+	}
 })
 
-router.get('/product/:name(*)', (req, res) => {
+router.get('/product/:name(*)', async (req, res) => {
 	let name = req.params.name.replace(/\/$/, "") // Delete the last '/' from the param.name
-	funcMongo.GetOneProduct(name).then((result) => {
-		if (result) { // have categories
-			//let data = convertData(result);
-			res.render('product', { prod: result, title: result.name });
-		}
-		else {
-			res.json({ status: false })
-		}
-	});
+	const result = await funcMongo.GetOneProduct(name);
+	if (result) {
+		res.render('product', { prod: result, title: result.name });
+	}
+	else { //not exist
+		res.json({ status: false })
+	}
 })
 
 
