2c1ee6f8c6f9eb30200cf41c7baad19b1359440e
Nicolas Castillo (TheMasterNico) <nscastillo.h@gmail.com>
2020-08-01 22:05:50 -0500

    Convert urls to API

--- routes/index.js
+++ routes/index.js
@@ -3,27 +3,31 @@ let router = express.Router();
 
 let funcMongo = require('../api/mongo');
 
-router.get('/', async (req, res, next) => {
-	const result = await funcMongo.GetCategories(null);
-	res.render('categories', { categories: result, title: 'Comparando precios' });
-});
-
-router.get('/cat/:category(*)', async (req, res) => {
-	const result = await funcMongo.GetCategories(req.params.category);
+router.get(['/', '/cat/:category(*)'], async (req, res) => {
+	res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+	let the_cat = null;
+	if (req.params.category) {
+		the_cat = req.params.category.replace(/\/$/, "") // Delete the last '/' from the param.category
+	}
+	const result = await funcMongo.GetCategories(the_cat);
 	if (result.length) { // have more categories
-		res.render('categories', { categories: result, title: 'Comparando precios' });
+		res.json({result, prefix_url: 'cat'});
+		//res.render('categories', { categories: result, title: 'Comparando precios' });
 	}
 	else {
-		const result_prod = await funcMongo.GetProducts(req.params.category);
-		res.render('products', { prod: result_prod, title: req.params.category });
+		const result_prod = await funcMongo.GetProducts(the_cat);
+		res.json({ result: result_prod, prefix_url: 'product'});
+		//res.render('products', { prod: result_prod, title: req.params.category });
 	}
 })
 
 router.get('/product/:name(*)', async (req, res) => {
+	res.header("Access-Control-Allow-Origin", "http://localhost:3000");
 	let name = req.params.name.replace(/\/$/, "") // Delete the last '/' from the param.name
 	const result = await funcMongo.GetOneProduct(name);
 	if (result) {
-		res.render('product', { prod: result, title: result.name });
+		res.json({ result, prefix_url: false });
+		//res.render('product', { prod: result, title: result.name });
 	}
 	else { //not exist
 		res.json({ status: false })
