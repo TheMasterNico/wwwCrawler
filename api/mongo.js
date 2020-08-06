b302c096f5cfe44ea40cf3838129d32923b1c4cc
Nicolas Castillo (TheMasterNico) <nscastillo.h@gmail.com>
2020-08-05 22:53:09 -0500

    Resturn correct data

--- api/mongo.js
+++ api/mongo.js
@@ -7,7 +7,6 @@ const productsColl = 'Items';
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
-
 let GetCategories = async (parentCat) => {
 	let db = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 	let coll = db.db(mongoDB).collection(collectionDB);
@@ -25,7 +24,8 @@ let GetProducts = async (Cat) => {
 let GetOneProduct = async (name) => {
 	let db = await MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 	let coll = db.db(mongoDB).collection(productsColl);
-	let result = coll.findOne({ 'name': name });
+	let result = await coll.findOne({ 'name': name });
+	//result = ;
 	return convertData(result);
 }
 
@@ -33,10 +33,17 @@ function currencyFormat(num) {
 	return '$' + num.toFixed().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1.')
 }
 
-let convertData = (arr) => {
+let convertData = async (arr) => {
 	for (let i in arr.prices) {
-		arr.prices[i].new_price = currencyFormat(arr.prices[i].new_price);
+		if (arr.prices[i].old_price) { //if have oldprice, adding discount
+			arr.prices[i].discount = Math.trunc(arr.prices[i].new_price * 100 / arr.prices[i].old_price).toString() + "%";
+		}
+		arr.prices[i].old_price = currencyFormat(arr.prices[i].old_price); // Convert prices
+		arr.prices[i].new_price = currencyFormat(arr.prices[i].new_price); // Convert prices
+		arr.prices[i].date = new Date(arr.prices[i].date * 1000).toString(); // Convert date like "Sun Aug 02 2020 21:54:44 GMT-0500 (Eastern Standard Time)"
+
 	}
+	console.log(arr);
 	return arr;
 }
 
